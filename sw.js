2d665c3da42faa7037401e096ab717f00cc20ed2
Dennis Senior <dennis.senior@ncssuite.com>
2026-06-04 14:18:29 -0600

    Update cache version from 'winco-v1' to 'winco-v2'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'winco-v1';
+const CACHE = 'winco-v2';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
